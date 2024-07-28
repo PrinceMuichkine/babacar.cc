1f8d0b5e54eb7050728132f595213a082f514176
PrinceMuichkine <babacar.diop.bba@edhec.com>
2024-07-28 23:13:27 +0000

    last update

--- pages/index.tsx
+++ pages/index.tsx
@@ -63,7 +63,7 @@ export default function Home() {
           description,
           images: [
             {
-              url: "https://babacar.cc/images/sharing.png",
+              url: "https://res.cloudinary.com/dzrdlevfn/image/upload/v1722208180/sharingg_oyxfz1.png",
               width: 450,
               height: 450,
               alt: title,
